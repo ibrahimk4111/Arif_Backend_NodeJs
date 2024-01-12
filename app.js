d53c71c28aa5f84e319a04b6ff8fb8f89cedc75b
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-01-12 07:47:32 +0600

    app.js updated recently by me

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/views'));
 
 app.set('view engine', 'ejs');
 // app.use()
 
 app.get('/', (req, res) => {
-  res.render('nav', {passText:"get text from app.js"})
+  // res.render('nav', {passText:"get text from app.js"})
+  res.status(200).json({message: "get db"})
 });
 
 
