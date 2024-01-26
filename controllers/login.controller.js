328dd791e5b065ea34dd1d8ff8278104ffb376b5
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-01-26 19:57:37 +0600

    reg and login

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,12 +1,20 @@
-const userLogInInterface = (req, res) =>{
-    res.render("loginForm", {title: "log in form"})
-}
+const userLogInInterface = (req, res) => {
+  res.render("loginForm", { title: "log in form" });
+};
 
-const loginUser = (req, res) =>{
-    res.json({msg:"user log in post method"})
-}
+const loginUser = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (email == "arifs@dhaka.net" && password == "arifs#bd@4111") {
+    // res.json({ msg: "user loged in successfully" });
+    res.redirect("/products")
+  }else{
+    res.render("loginForm", { title: "log in failed" });
+    res.redirect("/")
+  }
+};
 
 module.exports = {
-    userLogInInterface,
-    loginUser
-}
+  userLogInInterface,
+  loginUser,
+};
